7719e3fa17a584dcf15e92a4d8555e8c77248c1b
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2020-10-25 23:30:23 +0800

    Celcius to Fahrenheit

---
+++ CelciusToFahrenheit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float a;
+	float b;
+	float fahrenheit;
+		cout << "Enter temperature in Celcius: " ;
+		cin >> b;
+		fahrenheit = (b*1.8)+32;
+		cout << "Value of Fahrenheit: " << fahrenheit;
+}
