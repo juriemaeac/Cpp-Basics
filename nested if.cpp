5bc7a43fe5c26de96ed99fadaf0ba67772f6b5e3
Jurie Mae Castronuevo <59803167+juriemaeac@users.noreply.github.com>
2021-04-15 18:45:49 +0800

    if condition and while loop

---
+++ nested if.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int grade;
+	cout << "Enter grade:  ";
+	cin >> grade;
+	if (grade > 100){
+	cout << "INVALID INPUT";
+	} {
+		{
+		 if (90 <= grade && grade <= 100){
+		cout << "A";
+		}
+		{
+			 if (80 <= grade && grade <= 89 ){
+			cout << "B";}	
+			{
+					 if (70 <= grade && grade <= 79){
+					cout << "C";}
+				{
+					 if (60 <= grade && grade <= 69)
+					{
+					cout << "D";}
+						{
+						 if (grade < 60){
+						cout << "F";}
+						}
+				}
+			}
+		}
+		}
+	}
+}	
+
+	
+	
+	
+
---
+++ while loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num, start;
+	cout << "Enter a Positive Number:  ";
+	cin >> num;
+	start = 1;
+	while (start <= num){
+		cout << start<< " ";
+		start++;
+	} 
+	
+}
